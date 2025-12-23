2473489df1e54b37757e1f71cbecb4470dd2aba6
moviehall <moviehall50@gmail.com>
2025-12-24 00:35:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = ""
 
 # /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
